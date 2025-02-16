b529a9a1fc35fc376cf665a53c33dbd216ec1a82
User <maksimnagorny91@gmail.com>
2025-02-16 10:50:01 +0300

    дз-10

--- src/Main.java
+++ src/Main.java
@@ -1,114 +1,68 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("задание 1");
         /// task 1
-        int salary = 15000;
-        int total = 0;
-        int i = 1;
-        while (total < 2_459_000) {
-            i = i + 1;
-            total = total + salary;
-            System.out.println(i + " месяц, сумма накоплений равна " + total);
-        }
-        System.out.println(" задание 2");
+        int[] bug = new int[3];
+        bug[0] = 1;
+        bug[1] = 2;
+        bug[2] = 3;
+        double[] worm = {1.57, 7.654, 9.986};
+        int[] fly = {4, 40, 44};
         /// task 2
-        int g = 1;
-        while (g <= 10) {
-            System.out.print(g + " ");
-            g = g + 1;
+        for (int i = 0; i < bug.length; i++) {
+            if (i < bug.length - 1) {
+                System.out.print(bug[i] + ", ");
+            } else {
+                System.out.print(bug[i]);
+                System.out.println();
+            }
+        }
+        ///
+
+        for (int i1 = 0; i1 < worm.length; i1++) {
+            if (i1 < worm.length - 1) {
+                System.out.print(worm[i1] + ", ");
+            } else {
+                System.out.print(worm[i1]);
+                System.out.println();
+            }
+        }
+        ///
+        for (int i2 = 0; i2 < fly.length; i2++) {
+            if (i2 < fly.length - 1) {
+                System.out.print(fly[i2] + ", ");
+            } else {
+                System.out.print(fly[i2]);
+            }
         }
         System.out.println();
-        for (int d = 10; d >= 1; d = d - 1)
-            System.out.print(d + " ");
+        for (int r = bug.length - 1; r >= 0; r--) {
+            System.out.print(bug[r]);
+            if (r != 0) {
+                System.out.print(", ");
+            }
+        }
         System.out.println();
-        System.out.println(" задание 3 ");
-        /// task 3
-        int Y = 12_000_000;
-        int birth = 17;
-        int mortality = 8;
-        int on = 1000;
-        int year = 1;
-        while (year <= 10) {
-            Y = (birth - mortality) * on + Y;
-            System.out.println(" год " + year + ", численность населения состовляет " + Y);
-            year++;
-        }
-        System.out.println(" задание 4 ");
-        /// task 4
-        int deposit = 15000;
-        int target = 0;
-        int month = 1;
-        while (target < 12_000_000) {
-            target = target + deposit;
-            target = target + target / 100 * 7;
-            System.out.println(" месяц " + month + ", сумма накоплений " + target);
-            month++;
-        }
-        System.out.println(" задание 5 ");
-        /// task 5
-        int deposit1 = 15000;
-        int target1 = 0;
-        int month1 = 0;
-        while (target1 < 12_000_000) {
-            target1 = target1 + deposit1;
-            target1 = target1 + target1 / 100 * 7;
-            month1++;
-            if (month1 % 6 == 0) {
-                System.out.println(" месяц " + month1 + ", сумма накоплений " + target1);
+        for (int q = worm.length - 1; q >= 0; q--) {
+            System.out.print(worm[q]);
+            if (q != 0) {
+                System.out.print(", ");
             }
         }
-        System.out.println(" задание 6 ");
-        /// task 6
-        int moon = 0;
-        int sum = 15000;
-        int common = 0;
-        while (moon < 108) {
-            common = common + sum;
-            common = common + common / 100 * 7;
-            moon++;
-            if (moon % 6 == 0) ;
-            System.out.println(" на " + moon + " месяц сумма накопления ровна " + common);
-        }
-        System.out.println(" задание 7");
-        /// task 7
-        int firstFriday =7;
-        int date = 1;
-        while (date <= 31) {
-            if ((date - firstFriday) % 7 == 0) {
-                System.out.println(" сегодня число " + date + "," +
-                        " пятница, необходимо подготовить отчет");
-            }
-            date++;
-        }
-        System.out.println(" задание 8 ");
-        /// task 8
-        int yearsEarlier = 2025 - 200;
-        int yearsLater = 2025 + 100;
-        int years = 0;
-        for (years = yearsEarlier; years < yearsLater; years++) {
-            if (years % 79 == 0) {
-                System.out.println(years);}
+        System.out.println();
+        for (int z = fly.length - 1; z >= 0; z--) {
+            System.out.print(fly[z]);
+            if (z != 0) {
+                System.out.print(", ");
             }
         }
+        System.out.println();
+        for (int i = 0; i < bug.length; i++) {
+            if (bug[i] % 2 != 0) {
+                bug[i] += 1;
+            }
+        }
+        System.out.println(Arrays.toString(bug));
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
